2290a068cc52c807cedc7aacf466349c037b17a4
vunky-himawan <tingkerbeii44@gmail.com>
2024-06-29 20:04:28 +0700

    feat: add skeleton loading

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,14 +1,20 @@
 import { useEffect, useRef, useState } from "react";
 import Heading from "../Heading";
+import useLoading from "@/hooks/useLoading";
 
 const Hero = () => {
   const imageRef = useRef<HTMLImageElement>(null);
-  const images: string[] = [
-    "/assets/gif/lv_0_20240625225819-ezgif.com-loop-count.gif",
-    "/assets/images/DSC09725 (5).webp",
+  const [isLoading, setIsLoading] = useState(true);
+  const images = [
+    "lv_0_20240625225819-ezgif.com-loop-count.gif",
+    "DSC09725 (5).webp",
   ];
-
+  const [medias, setMedias] = useState<string[]>([]);
   const [image, setImage] = useState(1);
+
+  useEffect(() => {
+    useLoading(images, setMedias, setIsLoading, "/assets/images/");
+  }, []);
 
   const handleHover = () => {
     setImage((prev) => (prev === 0 ? 1 : 0));
@@ -16,7 +22,7 @@ const Hero = () => {
 
   useEffect(() => {
     if (imageRef.current) {
-      imageRef.current.src = images[image];
+      imageRef.current.src = medias[image];
     }
   }, [image]);
 
@@ -32,38 +38,45 @@ const Hero = () => {
                 stunning and user-friendly websites. Explore my projects to see
                 how I bring ideas to life with creative design.
               </p>
+              <div className="flex gap-x-3">
+                <a href="https://github.com/vunky-himawan">
+                  <div className="i-mdi:github"></div>
+                </a>
+                <a href="https://www.instagram.com/vunkyh">
+                  <div className="i-mdi:instagram"></div>
+                </a>
+                <a href="https://www.linkedin.com/in/vunky-himawan">
+                  <div className="i-mdi:linkedin"></div>
+                </a>
+                <a href="/assets/Vunky Himawan-resume.pdf">
+                  <div className="i-ic:round-description"></div>
+                </a>
+              </div>
             </Heading>
-            <div className="flex gap-x-3">
-              <a href="https://github.com/vunky-himawan">
-                <div className="i-mdi:github"></div>
-              </a>
-              <a href="https://www.instagram.com/vunkyh">
-                <div className="i-mdi:instagram"></div>
-              </a>
-              <a href="https://www.linkedin.com/in/vunky-himawan">
-                <div className="i-mdi:linkedin"></div>
-              </a>
-              <a href="/assets/Vunky Himawan-resume.pdf">
-                <div className="i-ic:round-description"></div>
-              </a>
-            </div>
           </div>
-          <div className="lg:rotate-5 relative group">
-            <div className="absolute top-4 -right-3 bg-red-5 p-2 text-white rounded-md rotate-30 text-[0.7rem] md:text-[1rem] z-2 lg:scale-0 lg:group-hover:scale-100 transition-all duration-300">
-              <h1>Frontend Web Developer</h1>
-            </div>
-            <div className="absolute bottom-4 -left-3 bg-red-5 p-2 text-white rounded-md rotate-30 text-[0.7rem] md:text-[1rem] z-2 lg:scale-0 lg:group-hover:scale-100 transition-all duration-300">
-              <h1>Web Designer</h1>
-            </div>
-            <img
-              ref={imageRef}
-              src={images[image]}
-              alt=""
-              loading="lazy"
-              className="w-full object-cover rounded-xl"
-              onMouseEnter={handleHover}
-              onMouseLeave={handleHover}
-            />
+          <div className={`${isLoading ? "" : "lg:rotate-5"} relative group`}>
+            {isLoading && (
+              <div className="w-full h-full bg-gray-100 rounded-xl animate-pulse"></div>
+            )}
+            {!isLoading && (
+              <>
+                <div className="absolute top-4 -right-3 bg-red-5 p-2 text-white rounded-md rotate-30 text-[0.7rem] md:text-[1rem] z-2 lg:scale-0 lg:group-hover:scale-100 transition-all duration-300">
+                  <h1>Frontend Web Developer</h1>
+                </div>
+                <div className="absolute bottom-4 -left-3 bg-red-5 p-2 text-white rounded-md rotate-30 text-[0.7rem] md:text-[1rem] z-2 lg:scale-0 lg:group-hover:scale-100 transition-all duration-300">
+                  <h1>Web Designer</h1>
+                </div>
+                <img
+                  src={medias[image]}
+                  ref={imageRef}
+                  onMouseEnter={handleHover}
+                  onMouseLeave={handleHover}
+                  alt=""
+                  loading="lazy"
+                  className="w-full object-cover rounded-xl"
+                />
+              </>
+            )}
           </div>
         </div>
       </section>
